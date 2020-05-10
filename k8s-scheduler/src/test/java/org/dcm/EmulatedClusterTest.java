88970b73080eef1b926882e2b7fd054b8ce3a1bc
Faria Kalim <faria.kalim@gmail.com>
2020-05-10 00:39:56 -0500

    bug fix
    Signed-off-by: Faria Kalim <kalim2@illinois.edu>

--- k8s-scheduler/src/test/java/org/dcm/EmulatedClusterTest.java
+++ k8s-scheduler/src/test/java/org/dcm/EmulatedClusterTest.java
@@ -25,8 +25,6 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Options;
-import org.jooq.Record;
-import org.jooq.Result;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
